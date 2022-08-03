86bfdd52462efafdd24b4b17f07321f71c123d9b
Erfan Motallebi <FS.C0d3rZ@gmail.com>
2022-08-03 19:15:46 +0430

    fixed the issue at server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const app = require("./server");
+const app = require("./app");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 const HOST = process.env.HOST || "localhost";
 
 app.listen(PORT, HOST, () => {
