fe56a4c19c0f5aa53ab23f9dfc1edce87f82b88d
Erfan Motallebi <FS.C0d3rZ@gmail.com>
2022-08-03 19:24:31 +0430

    added new types/resolvers

--- src/graphql/book.js
+++ src/graphql/book.js
@@ -1,10 +1,26 @@
 const { default: gql } = require("graphql-tag");
 const { bookz } = require("../data/db");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Book {
+    id: ID!
+    name: String!
+    genre: String!
+    yearOfRelease: Int!
+    authorId: Int!
+  }
+
+  type Query {
+    books: [Book!]!
+  }
+`;
 
 const resolvers = {
-  Query: {},
+  Query: {
+    books: (src, args, ctx, info) => {
+      return bookz;
+    },
+  },
 };
 
 module.exports = {
