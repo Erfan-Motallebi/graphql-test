b9aaad2477b9ed943a32d04022b15f755d5b5fb1
Erfan Motallebi <FS.C0d3rZ@gmail.com>
2022-08-07 13:54:12 +0430

    made some changes

--- src/__tests__/http/bookQuery.spec.js
+++ src/__tests__/http/bookQuery.spec.js
@@ -1,24 +1,22 @@
-const { describe, test, it } = require("@jest/globals");
-const { default: got } = require("got");
+const { describe, it } = require("@jest/globals");
+const { got } = require("got");
 const { GET_ALL_BOOKS_QUERY } = require("../queries/book.query");
 
 const baseURL = `http://localhost:4000/graphql`;
 
 describe("Query Tests", () => {
-  test("should see a list of books", async () => {
-    const listOfBooks = await (
-      await got(baseURL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          query: GET_ALL_BOOKS_QUERY,
-          variables: {},
-          operationName: null,
-        }),
-      })
-    ).json();
+  it("should see a list of books", async () => {
+    const listOfBooks = await got(baseURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: GET_ALL_BOOKS_QUERY,
+        variables: {},
+        operationName: null,
+      }),
+    });
 
     console.log(listOfBooks);
   });
